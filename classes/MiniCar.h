f7f55789e8241eadf0f618e5f940100e665fcd35
critzlez <jm.armstrong1207@gmail.com>
2019-05-16 14:51:49 -0700

    Added comments/Improved code

--- classes/MiniCar.h
+++ classes/MiniCar.h
@@ -9,6 +9,9 @@ class MiniCar : public Car
 		bool isMiniCooper;
 	
 	public:
+		// ": Car(...)" is the way to initialize the constructor of the superclass.
+		// This is another way to initialize variables too, but it isn't something
+		// we're going to talk about
 		MiniCar(std::string make, std::string model, std::string color,
 				int year, bool isMiniCooper) : Car(make, model, color, year)
 		{
